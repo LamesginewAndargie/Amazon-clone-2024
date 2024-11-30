37e0e480d32bb1b9ec6f0c8551f85db5b936240c
LamesginewAndargie <lame2002@gmail.com>
2024-11-30 10:14:09 +0300

    loading functionality is added

--- amazon-clone/src/Pages/ProductDetail/ProductDetail.jsx
+++ amazon-clone/src/Pages/ProductDetail/ProductDetail.jsx
@@ -5,24 +5,30 @@ import { productUrl } from "../../Api/endPoints";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import ProductCard from "../../Components/Product/ProductCard";
+import Loader from "../../Components/Loader/Loader";
 function ProductDetail() {
 	const { productId } = useParams();
-	// console.log(productId);
-	const [product, setProduct] = useState([]);
+	console.log(productId);
+	const [product, setProduct] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
 	useEffect(() => {
+		setIsLoading(true);
 		axios
 			.get(`${productUrl}/products/${productId}`)
 			.then((res) => {
 				// console.log(res);
 				setProduct(res.data);
+				setIsLoading(false);
 			})
 			.catch((err) => {
 				console.log(err);
+				setIsLoading(false);
 			});
 	}, []);
 	return (
 		<Layout>
-			<ProductCard product={product} />
+			{isLoading ? <Loader /> : <ProductCard product={product} />}
+			{/* <ProductCard product={product} /> */}
 		</Layout>
 	);
 }
--- amazon-clone/src/Pages/Results/Results.jsx
+++ amazon-clone/src/Pages/Results/Results.jsx
@@ -5,19 +5,24 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { productUrl } from "../../Api/endPoints";
 import ProductCard from "../../Components/Product/ProductCard";
+import Loader from "../../Components/Loader/Loader";
 function Results() {
 	const [products, setProducts] = useState([]);
 	const { categoryName } = useParams();
+	const [isLoading, setIsLoading] = useState(false);
 	// console.log(categoryName);
 	useEffect(() => {
+		setIsLoading(true);
 		axios
 			.get(`${productUrl}/products/category/${categoryName}`)
 			.then((res) => {
 				// console.log(res.data);
 				setProducts(res.data);
+				setIsLoading(false);
 			})
 			.catch((err) => {
 				console.log(err);
+				setIsLoading(false);
 			});
 	}, []);
 
@@ -27,11 +32,15 @@ function Results() {
 				<h1 style={{ padding: "30px" }}>Results</h1>
 				<p style={{ padding: "30px" }}>Category/{categoryName}</p>
 				<hr />
-				<div className={classes.products_container}>
-					{products?.map((product) => (
-						<ProductCard key={product.id} product={product} />
-					))}
-				</div>
+				{isLoading ? (
+					<Loader />
+				) : (
+					<div className={classes.products_container}>
+						{products?.map((product) => (
+							<ProductCard key={product.id} product={product} />
+						))}
+					</div>
+				)}
 			</section>
 		</Layout>
 	);
